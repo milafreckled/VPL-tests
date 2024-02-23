4e9b2df916ef77672035b27eeb0af750eb8aab13
Liudmyla Malomuzh <liudmyla.malomuzh@pollub.edu.pl>
2024-02-23 12:53:12 +0000

    Updated HomePage.java

--- src/main/java/org/example/PageObjects/HomePage.java
+++ src/main/java/org/example/PageObjects/HomePage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import framework.LogWriter;
+import org.example.framework.LogWriter;
 
 public class HomePage {
 
